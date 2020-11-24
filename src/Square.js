eb039548c1b7b3539b73640409fbdd47f56de88f
HyungJune <shinhj2512@naver.com>
2020-11-24 17:21:57 +0900

    my branch dev

---
+++ src/Square.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: this.props.value,
+        };
+    }
+
+    render() {
+        return (
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+export default Square;
